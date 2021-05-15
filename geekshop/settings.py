40d42f377fd1cbcfdc97c7b76e82d19486d65451
Ramis Khasianov <ramis.khasianov@gmail.com>
2021-05-15 21:34:13 +0300

    profile and profile images

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -132,3 +132,7 @@ STATICFILES_DIRS = (
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'authapp.User'
+LOGIN_URL = '/users/login/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
